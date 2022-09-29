760bfc1d1d160171c750760b711da4ac7bf8e21d
IsmoiljonJabborov <190107106@stu.sdu.edu.kz>
2022-09-29 11:01:11 +0500

    I am having difficulties while understanding the tasks. These tasks re somehow unclear!

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        int a = 9;
+        int b = 5;
+        int c = 2;
+        int d = 7;
+        int e = 4;
+        char a1 = 'a';
+        char b1 = 'b';
+
+        int formula = (a * (a1 * a1) - b * b1 + c + a1 - d) * ((a1 + b1 - e * a1 * b1) / c );
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+
+        System.out.println((int) c + " " + (int) h + " " + (int) a + " "  + (int) r);
     }
 }
